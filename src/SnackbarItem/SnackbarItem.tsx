e6e292d7a6d49e73d2e36a8b511bcf0f60915ab7
Ross Bernard Jamaca <58159983+rbjamaca@users.noreply.github.com>
2020-11-18 14:55:12 +0800

    Update SnackbarItem.tsx

--- src/SnackbarItem/SnackbarItem.tsx
+++ src/SnackbarItem/SnackbarItem.tsx
@@ -41,7 +41,7 @@ const styles = (theme: Theme) => {
             color: theme.palette.getContrastText(backgroundColor),
             alignItems: 'center',
             padding: '6px 16px',
-            borderRadius: '4px',
+//             borderRadius: '4px',
             boxShadow: '0px 3px 5px -1px rgba(0,0,0,0.2),0px 6px 10px 0px rgba(0,0,0,0.14),0px 1px 18px 0px rgba(0,0,0,0.12)',
         },
         message: {
